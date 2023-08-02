75f79b2cfbaa7f0f6e4585c8a0e449294a154b9a
debabrata-pw08-429 <chintuthecoder@gmail.com>
2023-08-02 18:19:52 +0530

    Form Edit and Delete Functionality Added with backend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,18 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { get_FormDetails } from "../ReduxStore/_singleForm/actions";
 
+const emptyForm = {
+  header: { title: "New Form", description: "", imageURL: "" },
+  sections: [],
+};
+
+const apiUrl = `${import.meta.env.VITE_SOME_apiURL}/forms`;
+
 const Home = () => {
   const dispatch = useDispatch();
   const [forms, setForms] = useState([]);
 
   useEffect(() => {
-    const apiUrl = `${import.meta.env.VITE_SOME_apiURL}/forms`;
-
     async function fetchData() {
       // Function to fetch data from the API
       try {
@@ -24,7 +29,7 @@ const Home = () => {
 
     // Call the fetchData function when the component mounts
     fetchData();
-  }, []);
+  }, [forms]);
 
   // Function to handle the "Edit" button click
   const handleEdit = (editData) => {
@@ -35,13 +40,45 @@ const Home = () => {
     }
   };
 
+  const addNewForm = () => {
+    setForms((prevForms) => [...prevForms, { ...emptyForm }]);
+
+    async function sendFormData() {
+      try {
+        const response = await axios.post(apiUrl, emptyForm);
+        const data = await response.data;
+        setForms([...data]);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+
+    sendFormData();
+  };
+
+  const handleFormDeletion = async (formId) => {
+    try {
+      // Make a DELETE request to the API to delete the form with the given formId
+      const response = await axios.delete(`${apiUrl}/${formId}`);
+
+      // If the deletion is successful, remove the form from the local state
+      if (response.status === 200) {
+        setForms((prevForms) =>
+          prevForms.filter((form) => form._id !== formId)
+        );
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <div className="home">
       <div>
-        {forms.length <= 0 ? (
+        {forms?.length <= 0 ? (
           <h1>No Forms Available</h1>
         ) : (
-          forms.map((ele, idx) => {
+          forms?.map((ele, idx) => {
             return (
               <div key={idx} className="home_form_collections">
                 <div className="home_form_01">
@@ -51,7 +88,10 @@ const Home = () => {
                 <div>
                   {/* Add an onClick event to handleEdit when "Edit" button is clicked */}
                   <button onClick={() => handleEdit(ele)}>Edit</button>
-                  <button>Delete</button>
+                  {/* Pass the form._id to the handleFormDeletion function */}
+                  <button onClick={() => handleFormDeletion(ele._id)}>
+                    Delete
+                  </button>
                 </div>
               </div>
             );
@@ -59,9 +99,7 @@ const Home = () => {
         )}
       </div>
       {/* Add a clickable element to redirect to the edit page */}
-      <div onClick={() => (window.location.href = "./edit")}>
-        + Add New Form
-      </div>
+      <div onClick={addNewForm}>+ Add New Form</div>
     </div>
   );
 };
