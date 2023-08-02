e92f2c579f0e8d4b0ef0db67c43766fdacb910bb
debabrata-pw08-429 <chintuthecoder@gmail.com>
2023-08-02 14:03:51 +0530

    Header ImageURL bugs fixing V1

--- src/components/form_header/Header.jsx
+++ src/components/form_header/Header.jsx
@@ -1,70 +1,87 @@
-// Import necessary modules and components
-import { useEffect, useRef, useState } from "react"; // React hooks for handling state and side effects
-import { RiImageAddFill } from "react-icons/Ri"; // Icon component for image upload
-import { useDispatch, useSelector } from "react-redux"; // Redux hooks for accessing the store
-
-// Import Redux actions
-import { set_HeaderDetails } from "../../ReduxStore/_singleForm/actions";
-
-// Import styles
+import { useEffect, useRef, useState } from "react";
+import { RiImageAddFill } from "react-icons/Ri";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetch_FormDetails,
+  set_HeaderDetails,
+} from "../../ReduxStore/_singleForm/actions";
 import "./headerModule.css";
+import axios from "axios";
 
-// Header component definition
 const Header = () => {
-  // Redux setup
-  const dispatch = useDispatch(); // Redux dispatcher
-  const headerData = useSelector((state) => {
-    return state.formReducer.header; // Get header data from Redux store
+  const dispatch = useDispatch();
+  const initHeader = useSelector((state) => state.formReducer.header);
+
+  const [headerFiles, setHeaderFiles] = useState([]);
+  const fileInputRef = useRef(null);
+  const headerDataRef = useRef({});
+
+  // Set the initial state of headerData using initHeader values or empty strings
+  const [headerData, setHeaderData] = useState({
+    title: initHeader?.title || "",
+    description: initHeader?.description || "",
+    imageURL: initHeader?.imageURL || "",
   });
 
-  // React refs for file input and header data
-  const fileInputRef = useRef(null); // Reference to the file input element
-  const headerDataRef = useRef(headerData); // Reference to the header data
-
-  // Local state for storing uploaded image files
-  const [headerFiles, setHeaderFiles] = useState([]);
-
-  // Function to handle adding an image
   const handleAddImg = () => {
-    fileInputRef.current.click(); // Trigger the file input click event
+    fileInputRef.current.click();
   };
 
-  // Side effect using useEffect to handle image extraction and update Redux store
   useEffect(() => {
-    // Function to extract image URL from the uploaded files and update header data
     const extractImg = () => {
       const filesArray = Array.from(headerFiles);
       const previewImg = filesArray.map((file) => ({
         imgUrl: URL.createObjectURL(file),
       }));
 
-      const img = previewImg[0]
-        ? previewImg[0].imgUrl
-        : headerDataRef.current.imageURL;
-
-      headerDataRef.current = { ...headerDataRef.current, imageURL: img };
+      const img = previewImg[0] ? previewImg[0].imgUrl : headerData.imageURL;
+      headerDataRef.current = { ...headerData, imageURL: img };
     };
 
-    // Call the extractImg function
-    extractImg();
+    const pathname = window.location.pathname;
+    const formId = pathname.split("/").pop();
+    const apiUrl = `${import.meta.env.VITE_SOME_apiURL}/edit/${formId}`;
 
-    // Dispatch the updated header data to the Redux store
-    dispatch(set_HeaderDetails(headerDataRef.current));
-  }, [headerFiles, dispatch]);
+    async function fetchData() {
+      try {
+        const response = await axios.get(apiUrl);
+        const data = response.data;
+
+        if (data) {
+          dispatch(fetch_FormDetails(data));
+        }
+
+        if (data.header) {
+          headerDataRef.current = data.header;
+
+          // Update headerData state with the fetched data
+          setHeaderData({
+            title: data.header.title || "",
+            description: data.header.description || "",
+            imageURL: data.header.imageURL || "",
+          });
+
+          // Dispatch the updated header data to the store
+          dispatch(set_HeaderDetails(data.header));
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+
+    fetchData();
+    extractImg();
+  }, [dispatch, headerFiles]);
 
   return (
     <>
       <div className="header">
-        {/* Container for displaying the header image */}
         <div
           className="header_image"
           onClick={handleAddImg}
           style={{ backgroundImage: `url(${headerData.imageURL})` }}
         >
-          {/* Show a "+" icon if there is no image */}
           {headerData.imageURL ? "" : <RiImageAddFill className="cover-img" />}
-
-          {/* Hidden file input for uploading images */}
           <input
             type="file"
             name="headerImg"
@@ -74,9 +91,7 @@ const Header = () => {
             onChange={(e) => setHeaderFiles(e.target.files)}
           />
         </div>
-        {/* Container for displaying header details */}
         <div className="header_details">
-          {/* Input field for the header title */}
           <input
             name="title"
             className="header_title"
@@ -84,25 +99,23 @@ const Header = () => {
             value={headerData.title}
             placeholder="From title"
             onChange={(e) =>
-              dispatch(
-                set_HeaderDetails({ ...headerData, title: e.target.value })
-              )
+              setHeaderData({
+                ...headerData,
+                title: e.target.value,
+              })
             }
           />
           <hr />
-          {/* Input field for the header description */}
           <input
             name="description"
             className="header_description"
             value={headerData.description}
             placeholder="Form description"
             onChange={(e) =>
-              dispatch(
-                set_HeaderDetails({
-                  ...headerData,
-                  description: e.target.value,
-                })
-              )
+              setHeaderData({
+                ...headerData,
+                description: e.target.value,
+              })
             }
           />
         </div>
